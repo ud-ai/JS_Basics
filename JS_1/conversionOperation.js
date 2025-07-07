a1c55b46f0d96d469061191f41c85de24ff8e171
ud-ai <udaykant180@gmail.com>
2025-07-07 22:58:07 +0530

    feat: Add conversionOperation.js

---
+++ JS_1/conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="udya"
+
+/*console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+/*
+"43"=> 43
+"43abc"=> NaN
+true=> 1
+false=> 0
+null=> 0
+undefined=> NaN */
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+/*
+1=> true
+0=> false   
+""=> false
+"uday" => true
+*/
+let number=45
+let stringNumber = String(number)
+console.log(typeof stringNumber);
