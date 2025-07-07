7e12d1a2155b2c1366d5222c92d50313dcfa690b
ud-ai <udaykant180@gmail.com>
2025-07-07 22:31:42 +0530

    Add variable.js

---
+++ JS_1/variable.js
@@ -0,0 +1,16 @@
+const account_Id = 9876
+let accountEmail= "udaykant@gmail.com"
+var accountPassword = "uday1234"
+accountCity="Giridih"
+let accountState = "Jharkhand"
+
+//account_Id=2// not allowed as const cannot be reassigned
+
+console.log(account_Id);
+accountEmail = "abc@g.com"
+accountPassword = "abc1234"
+accountCity = "Ranchi"
+console.table([accountEmail, accountPassword, accountCity,accountState]);
+/*
+ 
+*/
