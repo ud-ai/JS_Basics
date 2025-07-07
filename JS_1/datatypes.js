7abf0f2db4e9ee338e08ccb4e2ce94c3e467cc95
ud-ai <udaykant180@gmail.com>
2025-07-07 22:46:11 +0530

    feat: Add datatypes.js

---
+++ JS_1/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+//alert(3+3); we are using node js, not browser
+
+console.log(3 + 3)
+  console.log("Hello World");
+  let name="Udaykant";
+  let age= 25;
+  let isLoggedIn = true;
+
+  /* number ==> 2^53
+  bigint
+  string==>"
+   boolean==> true/false
+   null==> empty value ()
+   undefined=>
+   symbol=> unique
+   object */
+
+   console.log(typeof undefined);// => undefined
+   console.log(typeof null);// => object
