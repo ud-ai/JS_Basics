ccdcca1595facb8e6434b50b58e9db8d486bfa02
ud-ai <udaykant180@gmail.com>
2025-07-07 23:09:04 +0530

    Fix: Correct type conversion and increment operations

--- JS_1/conversionOperation.js
+++ JS_1/conversionOperation.js
@@ -12,15 +12,43 @@ true=> 1
 false=> 0
 null=> 0
 undefined=> NaN */
-let isLoggedIn = 1
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+
+// let isLoggedIn = 1
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
 /*
 1=> true
 0=> false   
 ""=> false
 "uday" => true
 */
-let number=45
-let stringNumber = String(number)
-console.log(typeof stringNumber);
+
+// let number=45
+// let stringNumber = String(number)
+// console.log(typeof stringNumber);
+
+// let value=3
+// let negValue = -value
+// console.log(negValue); // -3
+
+let str1= "uday"
+let str2= "kant"
+let str3 = str1 + " " + str2
+console.log(str3); // uday kant
+
+// console.log("1"+2)// "12"
+// console.log(1+"2")// "12"
+// console.log("1"+2+2)// "122"
+// console.log(1+2+"2")// "32"
+
+// console.log(+true)// 1
+// console.log(+false)// 0
+// console.log(+null)// 0
+
+// let num1,num2,num3
+// num1=num2=num3=3+4
+
+let gameCounter=100
+++gameCounter ;
+gameCounter ++;
+console.log(gameCounter); // 101
