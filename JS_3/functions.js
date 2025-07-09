a634056ff6cdf91f1e81c1f09d9549e1c743a9fb
ud-ai <udaykant180@gmail.com>
2025-07-09 20:14:48 +0530

    feat: Add functions demonstrating rest parameters and object handling

--- JS_3/functions.js
+++ JS_3/functions.js
@@ -31,3 +31,31 @@ function loginusermessage(username="sam"){
 }
 console.log(loginusermessage("Uday Kant")); // Uday Kant just logged in
 console.log(loginusermessage())//undefined just logged in
+
+
+//when we don't know about the number of arguments that will be passed to the function, we can use rest operator
+function calccartprice(val1, val2,...prices){
+    return prices
+
+
+}
+console.log(calccartprice(100, 200, 300)); // [300](100, 200 are ignored as they are not part of the rest parameter)
+
+const user = {
+    username: "Uday Kant",
+    prices:199
+}
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.prices}`);
+}
+handleobject(user); // Username is Uday Kant and price is 199
+handleobject({
+    username:"uday",
+    price:299
+}); // Username is uday and price is 299
+
+const myarray = [1, 2, 3, 4, 5];
+function return2ndvlaue(getarray){
+    return getarray[1]
+}
+console.log(return2ndvlaue(myarray)); // 3
