55168023a680466cc91d5a04a2395d075151ca7e
ud-ai <udaykant180@gmail.com>
2025-07-10 20:18:49 +0530

    Fix: Removed unnecessary console logs and added function scope examples

--- JS_3/scopes.js
+++ JS_3/scopes.js
@@ -3,7 +3,7 @@ let a=300
 if (true){
     let a=10
 const b=20
-console.log("Inner: "+a); // 10
+//console.log("Inner: "+a); // 10
 
 }
 
@@ -12,7 +12,39 @@ console.log("Inner: "+a); // 10
 // const is used when we don't want to change the value of the variable
 // let is used when we want to change the value of the variable
 
-console.log(a); // 10
+//console.log(a); // 10
 // console.log(b); // 20
 // console.log(c); // 30
 
+function one(){
+    const username = "John";
+    function two(){
+        const website="youtube";
+        //console.log(username); // John
+    }
+    //console.log(website);
+    two();
+}
+one();
+
+if(true){
+    const username = "uday";
+    if(username === "uday"){
+        const website=" youtube";
+        //console.log(username+website); // uday youtube
+    }
+    //console.log(website); // ReferenceError: website is not defined
+}
+// console.log(username)
+
+// +++++++++++++++++ INTERESTING(IMP) +++++++++++++++++++++
+console.log(addone(5))// 6
+function addone(num){
+    return num+1
+}
+
+//console.log(addtwo(5)) // ReferenceError: addtwo is not defined(This is because addtwo is a function expression and not a function declaration)
+const addtwo = function(num){//expression
+    return num+2
+}
+addtwo(5)// 7
